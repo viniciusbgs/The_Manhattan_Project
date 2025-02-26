fb9447e4f48952b93128dab7c304a38d3ac1c168
tutryx <ulisesdeca@gmail.com>
2025-02-26 11:05:29 -0300

    Created data directory

--- app.py
+++ app.py
@@ -1,15 +1,34 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, url_for, g
+import sqlite3
 import pandas as pd
 
 app = Flask(__name__)
 
 # Carregar dados
+DATABASE = '.\\Data\\data.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 try:
     df = pd.read_csv("data.csv")
     print("Dados carregados com sucesso!")
 except Exception as e:
     print("Erro ao carregar dados:", e)
     df = pd.DataFrame()
+
+@app.route()
+def hello(name):
+    return url_for('get_all_data')
 
 @app.route('/')
 def index():
